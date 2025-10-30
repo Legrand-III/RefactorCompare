a992c23696f695aac3fa90209ad13bb13deb49cb
Legrand <legrandd@buffalo.edu>
2025-10-30 13:41:46 -0400

    created simple function to compare

---
+++ Math.py
@@ -0,0 +1,13 @@
+#addition
+def add(num1, num2):
+    return num1+num2
+
+#refactor w/ same answer
+#def add(num1, num2):
+#    sum = num1 + num2
+#    return sum
+
+#refactor w/ different answer
+#def add(num1, num2):
+#    sum = num1 + num2
+#    return num2 #returning second input instead of answer
